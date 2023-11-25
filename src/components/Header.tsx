3eadebb8f3235162ac98ae9712b6f780ab855b0a
taqiyyaghazi <taqiyyaghazi@gmail.com>
2023-11-25 16:22:15 +0700

    Create example blog

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <nav className="layout flex items-center justify-between py-4">
+      <ul className="flex items-center justify-between space-x-3 text-xs md:space-x-4 md:text-base">
+        <li>
+          <Link href="/" className="hover:underline">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/#posts" className="hover:underline">
+            Posts
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/lib/utils.ts
@@ -0,0 +1,5 @@
+export function absoluteUrl(path: string) {
+  return `${
+    process.env?.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'
+  }${path}`;
+}
